3d6c77777d031aa5b31e7c8c9ce2c664a6994bad
David Maher <sunookitsune@kitsunet.org>
2020-07-03 00:08:00 -0400

    Switched back to sync-fetch.

--- HttpClient.js
+++ HttpClient.js
@@ -1,8 +1,7 @@
-const axios = require('axios');
 const aesjs = require('aes-js');
 const crypto = require('crypto');
-const deasync = require('deasync-promise');
 const pkcs7 = require('pkcs7-padding');
+const fetch = require('sync-fetch');
 
 const G = 'A4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31266FEA1E5C41564B777E690F5504F213160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28AD662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24855E6EEB22B3B2E5';
 const P = 'B10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371';
@@ -52,7 +51,11 @@ class HttpClient {
         var data = {
             'diffie': a.getPublicKey('hex')
         };
-        var dh = deasync(axios.put('http://' + this.host + '/di/v1/products/0/security', data, this.config)).data;
+        var dh = fetch('http://' + this.host + '/di/v1/products/0/security', {
+            method: 'PUT',
+            body: JSON.stringify(data),
+            timeout: this.timeout
+        }).json();
         var s = a.computeSecret(dh['hellman'], 'hex', 'hex');
         var s_bytes = Buffer.from(s, 'hex').slice(0, 16);
         this.key = aesDecrypt(dh['key'], s_bytes).slice(0, 16);
@@ -61,12 +64,17 @@ class HttpClient {
 
     setValues = function(values) {
         var encrypted = encrypt(JSON.stringify(values), this.key);
-
-        deasync(axios.put('http://' + this.host + '/di/v1/products/1/air', encrypted, this.config));
+        fetch('http://' + this.host + '/di/v1/products/1/air', {
+            method: 'PUT',
+            body: encrypted,
+            timeout: this.timeout
+        });
     }
 
     #getOnce = function(endpoint) {
-        var resp = deasync(axios.get('http://' + this.host + endpoint, this.config)).data;
+        var resp = fetch('http://' + this.host + endpoint, {
+            timeout: this.timeout
+        }).text();
         return decrypt(resp, this.key);
     }
 
